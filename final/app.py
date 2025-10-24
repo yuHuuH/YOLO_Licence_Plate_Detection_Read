f03673ff6c72d1efdaf9ed6e57fc8490a6720b87
yuHuuH <huyho1608@gmail.com>
2025-10-24 23:02:48 +0700

    --allow-empty-message

--- final/app.py
+++ final/app.py
@@ -9,6 +9,30 @@ import threading
 import os
 import sys
 from collections import Counter
+import math
+from typing import Tuple, Union
+from deskew import determine_skew
+
+def rotate(
+        image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]]
+) -> np.ndarray:
+    old_width, old_height = image.shape[:2]
+    angle_radian = math.radians(angle)
+    width = abs(np.sin(angle_radian) * old_height) + abs(np.cos(angle_radian) * old_width)
+    height = abs(np.sin(angle_radian) * old_width) + abs(np.cos(angle_radian) * old_height)
+
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    rot_mat[1, 2] += (width - old_width) / 2
+    rot_mat[0, 2] += (height - old_height) / 2
+    return cv2.warpAffine(image, rot_mat, (int(round(height)), int(round(width))), borderValue=background)
+
+
+def get_deskew(image):
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    angle = determine_skew(grayscale)
+    rotated = rotate(image, angle, (0, 0, 0))
+    return rotated
 
 # --- PyInstaller Resource Path Helper ---
 def resource_path(relative_path):
@@ -33,6 +57,7 @@ def getLPText(plate_crop, plate_ocr):
     Extracts text from a license plate using OCR after correcting skew.
     """
     # Run OCR
+    plate_crop = get_deskew(plate_crop)
     text_plate = plate_ocr(plate_crop, imgsz=640, conf=0.5, verbose=False)
     ocr_pre = text_plate[0]
     id2char = plate_ocr.names
